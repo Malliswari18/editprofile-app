f0022c03da38dc63ea0c62fe260b13b88c1ae3bf
malliswari18 <malliswarimahendra@gmail.com>
2025-02-25 19:43:24 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3116329151"
+    serviceWorkerVersion: "2734793656"
   }
 });
